353abbe0e0146f5c0fdb85dc9f18b296df675087
Gopesh01-hub <gopeshgupta321@gmail.com>
2025-02-22 19:22:34 +0530

    Update PostFormCard.js

--- components/PostFormCard.js
+++ components/PostFormCard.js
@@ -72,8 +72,9 @@ export default function PostFormCard({onPost}){
             )}
             {uploads.length>0 && (
                 <div className="flex gap-2 mt-4 flex-wrap ">
-                    {uploads.map(upload  =>(
-                        <div className="">
+                    {uploads.map((upload,index)  =>(
+                        <div key={index} className="">
+                        
                             <img src={upload} alt="" className="w-auto h-24 rounded-md" />
                         </div>
                         
